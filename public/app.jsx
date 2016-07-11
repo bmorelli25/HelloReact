79e0d598d9aa447c65dfcdfd14354489c0dd0bcf
pentel13 <bmorelli25@gmail.com>
2016-07-11 15:43:05 -0700

    Added our first component
    First component Greeter added

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,16 @@
+//uppercase for react components
+var Greeter = React.createClass({ //one argument: options object, only thing that is required is a render method
+  render: function(){ //expects jsx code to be returned
+    return (
+      <div> //can only return ONE root element. That is why we have this div
+        <h1>Hello React!</h1>
+        <p>This is from the component.</p>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello React!</h1>,
+  <Greeter/>,
   document.getElementById('app')
 );

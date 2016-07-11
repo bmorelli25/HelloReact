3ccde5c9835a7382d84b69fd02b0a9fc31dab3c8
pentel13 <bmorelli25@gmail.com>
2016-07-11 16:01:55 -0700

    Added in component properties
    Two props added

--- public/app.jsx
+++ public/app.jsx
@@ -1,16 +1,27 @@
 //uppercase for react components
 var Greeter = React.createClass({ //one argument: options object, only thing that is required is a render method
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message.'
+    };
+  },
   render: function(){ //expects jsx code to be returned
-    return (
-      <div> //can only return ONE root element. That is why we have this div
-        <h1>Hello React!</h1>
-        <p>This is from the component.</p>
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return ( //can only return ONE root element. That is why we have this div
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
       </div>
     );
   }
 });
 
+var firstName = 'Brandon';
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message="Message from prop!"/>,
   document.getElementById('app')
 );
